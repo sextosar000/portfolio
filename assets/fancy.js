31163a65f70858140fee61ea5d5b8643977d4769
nextechcatapult <nextechcatapult@nextechpultsAir.localdomain>
2019-06-20 13:15:48 -0400

    Creation

--- assets/fancy.js
+++ assets/fancy.js
@@ -4,3 +4,15 @@ $(document).ready(function(){
       $(".textbub").fadeIn(750);
     });
   });
+
+
+  function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+    document.getElementById("main").style.marginLeft = "250px";
+    document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
+  }
+  
+  function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+    document.getElementById("main").style.marginLeft= "0";
+  }  
