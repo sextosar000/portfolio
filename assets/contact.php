fc381c4f0519c0fc6edb16c76e97581e1a5c66a3
nextechcatapult <nextechcatapult@nextechpultsAir.localdomain>
2019-06-24 12:07:57 -0400

    bork

--- assets/contact.php
+++ assets/contact.php
@@ -1,42 +1,39 @@
 <?php
- 
-// Email address verification
-function isEmail($email) {
-    return filter_var($email, FILTER_VALIDATE_EMAIL);
+
+$EmailFrom = "chriscoyier@gmail.com";
+$EmailTo = "snowykittin@.com";
+$Subject = "Nice & Simple Contact Form by CSS-Tricks";
+$Name = Trim(stripslashes($_POST['Name'])); 
+$Email = Trim(stripslashes($_POST['Email'])); 
+$Message = Trim(stripslashes($_POST['Message'])); 
+
+// validation
+$validationOK=true;
+if (!$validationOK) {
+  print "<meta http-equiv=\"refresh\" content=\"0;URL=error.htm\">";
+  exit;
 }
- 
-if($_POST) {
- 
-    // Enter the email where you want to receive the message
-    $emailTo = 'snowykittin@gmail.com';
- 
-    $clientEmail = addslashes(trim($_POST['email']));
-    $subject = addslashes(trim($_POST['subject']));
-    $message = addslashes(trim($_POST['message']));
-    $antispam = addslashes(trim($_POST['antispam']));
- 
-    $array = array('emailMessage' => '', 'subjectMessage' => '', 'messageMessage' => '', 'antispamMessage' => '');
- 
-    if(!isEmail($clientEmail)) {
-        $array['emailMessage'] = 'Invalid email!';
-    }
-    if($subject == '') {
-        $array['subjectMessage'] = 'Empty subject!';
-    }
-    if($message == '') {
-        $array['messageMessage'] = 'Empty message!';
-    }
-    if($antispam != '12') {
-        $array['antispamMessage'] = 'Wrong antispam answer!';
-    }
-    if(isEmail($clientEmail) && $subject != '' && $message != '' && $antispam == '12') {
-        // Send email
-        $headers = "From: " . $clientEmail . " <" . $clientEmail . ">" . "\r\n" . "Reply-To: " . $clientEmail;
-        mail($emailTo, $subject . " (bootstrap contact form tutorial)", $message, $headers);
-    }
- 
-    echo json_encode($array);
- 
+
+// prepare email body text
+$Body = "";
+$Body .= "Name: ";
+$Body .= $Name;
+$Body .= "\n";
+$Body .= "Email: ";
+$Body .= $Email;
+$Body .= "\n";
+$Body .= "Message: ";
+$Body .= $Message;
+$Body .= "\n";
+
+// send email 
+$success = mail($EmailTo, $Subject, $Body, "From: <$EmailFrom>");
+
+// redirect to success page 
+if ($success){
+  print "<meta http-equiv=\"refresh\" content=\"0;URL=credits.html\">";
 }
- 
+else{
+  print "<meta http-equiv=\"refresh\" content=\"0;URL=error.htm\">";
+}
 ?>
